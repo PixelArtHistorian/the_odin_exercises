034ef9ffd1a5abf2ad978a28acbf95493ae0afa3
Alberto <a.diana.dev@gmail.com>
2020-05-02 15:40:36 +0200

    Added solution for remove from array

--- removeFromArray/removeFromArray.js
+++ removeFromArray/removeFromArray.js
@@ -1,5 +1,18 @@
-const removeFromArray = function() {
-
+const removeFromArray = function(array) {
+    let args = Array.from(arguments);
+    if(args.length > 1){
+        for (let j = 1 ; j< args.length; j++){
+            for( let i = 0; i < array.length; i++){
+                if(array[i] === args[j]){
+                    array.splice(i,1);
+                }
+            }
+        }       
+    }
+    
+    return array;
 }
+
+removeFromArray([1, 2, 3, 4], 3);
 
 module.exports = removeFromArray
