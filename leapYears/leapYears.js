7b02ffb55ca063458cb60c91aa21ca84d774df81
Alberto <a.diana.dev@gmail.com>
2020-05-16 14:57:59 +0200

    Committed leap years solution

--- leapYears/leapYears.js
+++ leapYears/leapYears.js
@@ -1,5 +1,11 @@
-const leapYears = function() {
-
+const leapYears = function(year) {
+    let result;
+    if(typeof year != 'number'){
+        result = 'ERROR';
+    }else{
+        result = (year%4 === 0 && year%100 != 0) || (year%400 ===0);
+    }
+    return result;
 }
 
 module.exports = leapYears
