3ed89e21ba216432dbd4e85e5882f8d70bc08f94
Alberto <a.diana.dev@gmail.com>
2020-07-12 17:58:30 +0200

    Committed palindromes solution

--- palindromes/palindromes.js
+++ palindromes/palindromes.js
@@ -1,5 +1,14 @@
-const palindromes = function() {
-
+const palindromes = function(palindrome) {
+    let tempString = palindrome.toLowerCase()
+    let characters = Array.from(tempString).filter(x => (x!==" " && x!=="," && x!==";" && x!=="." && x!=="!" && x !=="?"));
+    let charactersReversed = Array.from(tempString).filter(x => x!==" " && x!=="," && x!==";" && x!=="." && x!=="!" && x !=="?").reverse();
+    for(let i=0; i<characters.length; i++){
+        if(characters[i]!==charactersReversed[i]){
+            return false;
+        }
+    }
+    return true;
 }
 
 module.exports = palindromes
+console.log(palindromes("Racecar!"));
